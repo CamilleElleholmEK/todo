6a30fef2719341fd94c8cb4b69cb88be168ce4a6
Camille <caod0001@stud.ek.dk>
2026-09-08 13:44:04 +0200

    dynamic weather

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { loadJSON } from "/utils_lib.js";
+
 // Const
 const taskText = document.querySelector("#taskText");
 const dateField = document.querySelector("#date");
@@ -9,6 +11,35 @@ const doneList = document.querySelector("#doneList");
 const createBtn = document.querySelector("#create");
 const taskArray = [];
 const doneArray = [];
+const wwCodes = {
+  0: "clearsky_day.png",
+  1: "fair_day.png",
+  2: "partlycloudy_day.png",
+  3: "cloudy.png",
+  45: "fog.png",
+  48: "fog.png",
+  // it's raining again👇🏼
+  51: "lightrain.png",
+  53: "lightrain.png",
+  55: "lightrain.png",
+  56: "lightsleet.png",
+  57: "lightsleet.png",
+  61: "lightrain.png",
+  63: "rain.png",
+  65: "heavyrain.png",
+  66: "lightsleet.png",
+  67: "lightsleet.png",
+  71: "lightsnow.png",
+  73: "snow.png",
+  75: "heavysnow.png",
+  77: "lightsnow.png",
+  80: "lightrainshowers_day.png",
+  81: "rainshowers_day.png",
+  82: "heavyrainshowers_day.png",
+  85: "lightsnowshowers_day.png",
+  86: "heavysnowshowers_day.png",
+  95: "rainandthunder.png",
+};
 
 // Eventlisteners
 document.querySelector("#create").addEventListener("click", create);
@@ -49,9 +80,18 @@ function create(e) {
     taskOutdoor: outdoorCheck.checked,
     id: self.crypto.randomUUID(),
   };
+
   if (e.currentTarget === createBtn) {
-    taskArray.push(taskObject);
-    displayList(taskArray, taskList);
+    loadJSON(
+      `https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.57&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto&start_date=${taskObject.taskDate}&end_date=${taskObject.taskDate}`,
+      (data) => {
+        taskObject.weatherCode = data.daily.weathercode[0];
+        taskArray.push(taskObject);
+        displayList(taskArray, taskList);
+      },
+    );
+    // taskArray.push(taskObject);
+    // displayList(taskArray, taskList);
   } else if (e.target.type === "checkbox") {
     const doneId = e.target.closest("li").dataset.id;
     if (
@@ -82,6 +122,7 @@ function displayList(arr, list) {
     const checkbox = document.createElement("input");
     const description = document.createElement("p");
     const date = document.createElement("p");
+    const weather = document.createElement("img");
     const del = document.createElement("button");
     const convertedDateY = task.taskDate.substring(0, 4);
     const convertedDateM = task.taskDate.substring(5, 7);
@@ -92,6 +133,7 @@ function displayList(arr, list) {
     description.innerHTML = `${task.taskText}`;
     date.innerHTML =
       convertedDateD + " / " + convertedDateM + " / " + convertedDateY;
+    weather.src = `png/${wwCodes[task.weatherCode]}`;
     del.innerHTML = "X";
 
     // Element class & data
@@ -108,7 +150,7 @@ function displayList(arr, list) {
     }
 
     // Insert Elements in li
-    li.append(checkbox, description, date, del);
+    li.append(checkbox, description, date, weather, del);
     list.appendChild(li);
   });
 
